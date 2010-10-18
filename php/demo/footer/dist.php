46a1d8f49495a768143e2b94b265dd2d389b5103
Cerdic <cedric@yterium.com>
2010-10-18 11:31:23 +0200

    documentation URL

--- php/demo/footer/dist.php
+++ php/demo/footer/dist.php
@@ -1,2 +1,2 @@
 This is footer section.
-More information about Z Framework <a href="http://www.yterium.net/">http://www.yterium.net/</a>
+More information about Z Framework <a href="http://www.yterium.net/108">http://www.yterium.net/108</a>
